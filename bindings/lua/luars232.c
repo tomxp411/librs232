tomxp411/librs232

commit 89aa0140a26dc7dad2e786cfe08877c7094622f4

Add. export `luaopen_rs232_core` function.

bindings/lua/luars232.c
@@ -518,7 +518,6 @@ static void create_metatables(lua_State *L, const char *name, const luaL_reg *me
 #endif
 }
 
-RS232_LIB int luaopen_luars232(lua_State *L);
 RS232_LIB int luaopen_luars232(lua_State *L)
 {
 	int i;
@@ -551,4 +550,8 @@ RS232_LIB int luaopen_luars232(lua_State *L)
 	    MODULE_VERSION, MODULE_BUILD, MODULE_TIMESTAMP);
 
 	return 1;
+}
+
+RS232_LIB int luaopen_rs232_core(lua_State *L){
+	return luaopen_luars232(L);
 }

.appveyor/install_com0com.bat
@@ -24,7 +24,6 @@ if not exist %COM0COM_ROOT%\setupc.exe (
   echo Install com0com
   echo ======================================================
   setup_com0com_W7_x64_signed.exe /S /D=%COM0COM_ROOT%
-  dir %COM0COM_ROOT%
 )
 
 echo ======================================================

commit c1115a78729706321e093925011323f66bfa2c5e

Fix compilation errors
Fixes the following error:
uars232.c:533:15: error: no previous prototype for 'luaopen_luars232' [-Werror=missing-prototypes]
  533 | RS232_LIB int luaopen_luars232(lua_State *L)
      |               ^~~~~~~~~~~~~~~~
luars232.c:567:15: error: no previous prototype for 'luaopen_rs232_core' [-Werror=missing-prototypes]
  567 | RS232_LIB int luaopen_rs232_core(lua_State *L){
      |               ^~~~~~~~~~~~~~~~~~
cc1: all warnings being treated as errors

The regression has been introduced by: https://github.com/srdgame/librs232/commit/89aa0140a26dc7dad2e786cfe08877c7094622f4

bindings/lua/luars232.c
@@ -530,6 +530,7 @@ static void create_metatables(lua_State *L, const char *name, const luaL_reg *me
 #endif
 }
 
+RS232_LIB int luaopen_luars232(lua_State *L);
 RS232_LIB int luaopen_luars232(lua_State *L)
 {
 	int i;
@@ -564,6 +565,7 @@ RS232_LIB int luaopen_luars232(lua_State *L)
 	return 1;
 }
 
+RS232_LIB int luaopen_rs232_core(lua_State *L);
 RS232_LIB int luaopen_rs232_core(lua_State *L){
 	return luaopen_luars232(L);
 }